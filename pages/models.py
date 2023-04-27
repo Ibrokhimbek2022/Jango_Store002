4f6f9c225b3244557337670e72801a629ea2a70e
student <student@intranet.proweb.uz>
2023-04-27 18:30:35 +0500

    added functions for subcategory and product detail

--- pages/models.py
+++ pages/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.urls import reverse
 
 # Create your models here.
 
@@ -22,6 +23,11 @@ class Subcategory(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("subcategory_articles", kwargs={"slug": self.slug})
+
+
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -55,6 +61,9 @@ class Product(models.Model):
     )
     subcategory = models.ForeignKey(Subcategory, on_delete=models.SET_NULL, verbose_name="Подкатегория", null=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name="Категория", null=True)
+
+    def get_absolute_url(self):
+        return reverse("product_detail", kwargs={"slug": self.slug})
 
     def get_class_by_type(self):
         TYPES_CLASSES = {
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from .models import Product, Category, Subcategory
+from django.core.paginator import Paginator
 # Create your views here.
 
 
@@ -7,9 +8,31 @@ def home_view(request):
     return render(request, "pages/index.html")
 
 
+
+
 def shop_view(request):
     products = Product.objects.all()
+    paginator = Paginator(products, 2)
+    page = request.GET.get("page")
+    result = paginator.get_page(page)
     context = {
-        "products": products
+        "products": result
     }
     return render(request, "pages/shop.html", context)
+
+
+def subcategory_articles_view(request, slug):
+    subcategory = Subcategory.objects.get(slug=slug)
+    products = Product.objects.filter(subcategory=subcategory)
+    paginator = Paginator(products, 2)
+    page = request.GET.get("page")
+    result = paginator.get_page(page)
+    context = {
+        "products": result
+    }
+    return render(request, "pages/shop.html", context)
+
+def product_detail_view(request, slug):
+    return render(request, "pages/product_detail.html")
+
+
